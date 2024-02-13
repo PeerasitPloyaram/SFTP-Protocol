2ebd85916c4e674bfa734ba45b0870547e12e07d
Peerasit <peerasitearth@outlook.com>
2024-02-13 10:08:24 +0700

    Add Command Argument and Input Output Format

--- client.py
+++ client.py
@@ -2,7 +2,8 @@ from socket import *
 from datetime import datetime
 from sftp import packet as packet
 from sftp import core as core
-import threading
+import sys
+import re
 import time
 import os
 
@@ -40,9 +41,45 @@ client_socket = socket(AF_INET, SOCK_DGRAM) # UDP
 client_socket.settimeout(timeOut)           # Set Time Out
 
 
+
+for arg in range(1,len(sys.argv)):  # Argument
+
+    if (sys.argv[arg] == "-p" or sys.argv[arg] == "--port"):
+        port = int(sys.argv[arg + 1])
+
+    elif (sys.argv[arg] == "-t" or sys.argv[arg] == "--test"):
+        con_packet = packet.createPCTPacket()   # Create PCT
+        try:    # Connection Establishment
+            terminal(f"-----Try to Connect to Server-----")
+            c_packet = con_packet.decode().split("/")
+            terminal(c_packet[0] + c_packet[1],"PUSH")
+            client_socket.sendto(con_packet,(server_name,port))     # Send PCT
+            message,server = client_socket.recvfrom(chunkSize)      # Wait CT Response
+            message = message.decode().split("/")                   # Divide Header and Content
+
+            if message[0] == operations["CT"]:              # Recive connection knownlagement
+                connect = True                              # Connected
+                connectId = message[1]                      # CT Sequence
+                terminal(message[0] + message[1], "GET")
+                if connect :
+                    exit(0)
+        
+        except timeout:
+            terminal("Can't connect to server because server not response.", operations["ERR"])
+            exit(1)
+
+    if re.match("[/a-zA-Z]",sys.argv[arg]):
+        file_name = sys.argv[arg]
+
+
+# file_name = "pic.png"
+# path = "/Users/peerasit/ku_study/network/projectProtocol/testing/"
+
+
 # Connection Phase
 con_packet = packet.createPCTPacket()   # Create PCT
 try:    # Connection Establishment
+    terminal(f"-----Try to Connect to Server-----")
     c_packet = con_packet.decode().split("/")
     terminal(c_packet[0] + c_packet[1],"PUSH")
     client_socket.sendto(con_packet,(server_name,port))     # Send PCT
@@ -56,20 +93,23 @@ try:    # Connection Establishment
 
 except timeout:
     terminal("Can't connect to server because server not response.", operations["ERR"])
-    exit()
+    exit(1)
 
 # Main
 if connect: # If Connect to Server
     terminal("-----Client Connected To Server-----")
 
     # Create file chunk
-    file = "/Users/peerasit/ku_study/network/projectProtocol/pic.png"
-    core.createChunk(core,file,chunk=chunkSize,verbose=False)    # Create packet to send
+    # file = path + file_name
+    file = file_name
+    path_file = file_name.split("/")
     file_id = 14
+    core.createChunk(core,file,chunk=chunkSize,verbose=False)    # Create packet to send
+
 
 
     temp_total_payload = core.getChunkCount(core)
-    for packet_number in range(10):
+    for packet_number in range(temp_total_payload):
         buffer = core.getChunkById(core, packet_number)
         id, pk = packet.createPacket(temp_total_payload,packet_number ,file_id, 1101,buffer)    
 
@@ -79,15 +119,8 @@ if connect: # If Connect to Server
 
         terminal(o)
     ##
-    client_socket.sendto(packet.createTFCPacket("png",temp_total_payload, file_id),(server_name, port))
+    client_socket.sendto(packet.createTFCPacket(path_file[len(path_file) - 1],temp_total_payload, file_id),(server_name, port))
     terminal("[TFC]","PUSH")
-
-
-
-    # # Wait Response from Server
-    # message,server = client_socket.recvfrom(4096) 
-    # header,content = message.split(b'/',1)
-    # header = header.decode()
 
     # Wait Response from Server
     while True:
@@ -97,7 +130,7 @@ if connect: # If Connect to Server
 
         match header:
             case "[RTP]":   # RTP Packet
-                terminal("[RTP]-> Packet Missing","GET")
+                terminal("[RTP]<- Packet Missing","GET")
 
                 content = content.decode()
                 id, num = content.split(":",1)  # Split to Id and List Number Packet
@@ -114,7 +147,7 @@ if connect: # If Connect to Server
                     o = f"PUSH Packet Id {id} Number {numberPacket}"
                     terminal(o)
             
-                client_socket.sendto(packet.createTFCPacket("png",temp_total_payload, file_id),(server_name, port)) # Send TFC Packet to Server
+                client_socket.sendto(packet.createTFCPacket(path_file[len(path_file) - 1],temp_total_payload, file_id),(server_name, port)) # Send TFC Packet to Server
                 terminal("[TFC]","PUSH")
 
             case "[END]":   # END Packet
@@ -124,27 +157,6 @@ if connect: # If Connect to Server
 
             case _: # Error
                 terminal("[OPERATION ERROR]")
-        
 
-
-# while True:
-#     message,server = client_socket.recvfrom(4096)
-#     message = message.decode().split("/")
-#     header_message = message[0]
-#     content_message = message[1]
-
-#     match header_message:
-#         case "[CT]":    # Recive connection knownlagement
-#             print("GET",header_message,content_message)
-        
-#         case "[END]":
-#             break
-#Send
-# for _ in range(core.getChunkCount(core)):          # Send packet
-#     payload = core.getChunkById(core,_)
-
-#     # print("\n",payload)
-
-#     client_socket.sendto(payload,(server_name,port))
 client_socket.close()
 
